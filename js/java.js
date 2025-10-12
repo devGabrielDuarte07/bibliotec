3acb9119084bba7b558b2bfa050db795e47fc785
gabrielDuarte <biel111107@gmail.com>
2025-10-11 23:21:36 -0300

    terminar de arrumar os link

---
+++ js/java.js
@@ -0,0 +1,13 @@
+const botaoAcao = document.getElementById('actionButton');
+const nome = document.getElementById("User");
+const senha = document.getElementById("senha");
+
+botaoAcao.addEventListener('click', function() {
+  // Validação dentro do evento
+  if (nome.value.trim() === "" || senha.value.trim() === "") {
+    alert("Por favor, preencha todos os campos!");
+    return;
+  }
+
+  window.location.href = "teste.html";
+});
