a3511cc1d7d7ba939715f30a0e61a205fac947ce
ninis333 <ninise755@gmail.com>
2025-11-13 11:15:40 -0300

    Arrumando as capas

--- js/carousel.js
+++ js/carousel.js
@@ -1,24 +1,37 @@
 const nextBtn = document.getElementById(`arrow-right`)
 const previousBtn = document.getElementById(`arrow-left`)
-let deslocamento = 0;
+
 const passo = 470;
 const limiteMax = -1880;
-const exibirLivros = document.querySelector('.exibirLivros');
+const categorias = ["Populares", "Mangas", "Romance", "Comedia", "Suspense", "Terror"]
+const carousel = document.querySelector('.apenasLivros');
+const limiteMin = 0;
 
-nextBtn.addEventListener(`click`, () => {
-  deslocamento -= passo;
-  
-   if (deslocamento < limiteMax) {
-    deslocamento = 0; // volta ao início
-  }
-  exibirLivros.style.transform = `translateX(${deslocamento}px)`;
+categorias.forEach((categoria) => {
+  const container = document.querySelector(`.exibirLivros.${categoria}`);
+  const carousel = container.querySelector('.apenasLivros');
+  const nextBtn = document.querySelector(`.controls${categoria} .arrow-right`);
+  const prevBtn = document.querySelector(`.controls${categoria} .arrow-left`);
+
+  let deslocamento = 0;
+
+  nextBtn.addEventListener(`click`, () => {
+    deslocamento -= passo;
+
+    if (deslocamento < limiteMax) {
+      deslocamento = 0; // volta ao início
+    }
+    carousel.style.transform = `translateX(${deslocamento}px)`;
+  })
+
+  prevBtn.addEventListener('click', () => {
+
+    deslocamento += passo;
+    if (deslocamento > 0) {
+      deslocamento = limiteMax; // vai pro final
+    }
+    carousel.style.transform = `translateX(${deslocamento}px)`;
+  })
 })
 
-previousBtn.addEventListener('click', () => {
 
-  deslocamento += passo;
-  if (deslocamento > 0) {
-    deslocamento = limiteMax; // vai pro final
-  }
-  exibirLivros.style.transform = `translateX(${deslocamento}px)`;
-})
